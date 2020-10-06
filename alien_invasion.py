73bfbb60cba7add4644d3ce58f0918992e10e44d
Luciano <oluciano.net@gmail.com>
2020-10-06 20:31:24 -0300

    Creating the fire_bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -15,10 +16,14 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     ship = Ship(ai_settings, screen)
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)        
+        
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
 
 run_game()
