09d3696e36345fc82fbcd52246e4e2bb2380c876
Luciano <oluciano.net@gmail.com>
2020-10-07 19:32:45 -0300

    Refactoring create_fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,8 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 
+from alien import Alien
+
 def run_game():
     pygame.init()
     ai_settings = Settings()
@@ -17,13 +19,19 @@ def run_game():
 
     ship = Ship(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    alien = Alien(ai_settings, screen)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)        
         
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
 
 
 run_game()
